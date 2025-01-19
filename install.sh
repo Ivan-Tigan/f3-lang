e278658a381ac66c87fbb1d55eae5b63b0558aa5
vankata <ivantsoninski@gmail.com>
2025-01-19 19:27:24 +0200

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget https://github.com/Ivan-Tigan/f3-lang/releases/download/v1.0.0/f3-lang_1.0_amd64.snap
 sudo snap install --dangerous f3-lang_1.0_amd64.snap
+sudo snap alias f3-lang.f3 f3
 rm f3-lang_1.0_amd64.snap
